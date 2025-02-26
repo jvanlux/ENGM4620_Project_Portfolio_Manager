03b408bedd0da9daccc8a5bb212195899cbe15e7
james <james.mcintosh@dal.ca>
2025-02-26 18:51:44 -0400

    Re_name file and move plotting to seperate file called Plot_Portfolio_Cash_and_Value.py

--- Cashflow_tracking.py
+++ Cashflow_tracking.py
@@ -1,8 +1,7 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 import datetime
 
-def read_and_plot_transactions(csv_file):
+def read_transactions(csv_file):
     # Read CSV file
     df = pd.read_csv(csv_file, header=None, names=["Type", "Symbol", "Price", "Shares", "Date"])
     df["Date"] = pd.to_datetime(df["Date"])
@@ -28,15 +27,8 @@ def read_and_plot_transactions(csv_file):
     dates.append(today)
     balances.append(balance)
 
-    # Plot the transactions
-    plt.figure(figsize=(10, 5))
-    plt.plot(dates, balances, drawstyle='steps-post', linestyle='dotted', color='red', marker='o')
-    plt.xlabel("Date")
-    plt.ylabel("Account Balance ($)")
-    plt.title("Investment Transactions Over Time")
-    plt.grid(True, linestyle='--', alpha=0.6)
-    plt.show()
+    return pd.DataFrame({"Date": dates, "Cash Balance": balances})
 
 # Call the function with the CSV file path
 csv_file = "my_trades.csv"
-read_and_plot_transactions(csv_file)
+read_transactions(csv_file)
